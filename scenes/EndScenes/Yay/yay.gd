9680e51cc637b21b5204dca6d5ee9d1c7ce99446
Robin OMeallie <robin.omeallie@gmail.com>
2025-09-28 03:17:20 -0400

    Lena's ending scenes transferred over, beginning on text box scene

--- scenes/EndScenes/Yay/yay.gd
+++ scenes/EndScenes/Yay/yay.gd
@@ -1,1 +1,13 @@
-extends Node
+extends Control
+
+# Path to your main menu scene
+const MAIN_MENU_SCENE := "res://Main_Menu.tscn"
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		_go_to_main_menu()
+	elif event is InputEventKey and event.pressed and event.scancode == KEY_SPACE:
+		_go_to_main_menu()
+
+func _go_to_main_menu() -> void:
+	get_tree().change_scene_to_file(MAIN_MENU_SCENE)
